72db40da4dbc5e98c199298c37881e03e40a9057
Maksim Kostromin <daggerok@gmail.com>
2018-03-03 01:54:25 +0200

    Initial commit

---
+++ src/main/java/daggerok/App.java
@@ -0,0 +1,11 @@
+package daggerok;
+
+import java.util.Arrays;
+
+import static java.lang.String.format;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println(format("daggerok context main stub %s", Arrays.asList(args)));
+  }
+}
